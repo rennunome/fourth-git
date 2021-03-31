db7fc2a2fe68d61cced1afe1051400c9024bb117
rennunome <r_nunome@emyus.jp>
2021-03-31 13:27:48 +0900

    Modified files for test marking function

--- src/main/java/com/emyus/TestController.java
+++ src/main/java/com/emyus/TestController.java
@@ -1,8 +1,12 @@
 package com.emyus;
 
+import java.security.Principal;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,7 +34,7 @@ public class TestController {
 	}
 
 	@PostMapping("/mark")
-	public String mark(@ModelAttribute ("questions_id") int[] questions_id, @ModelAttribute("test_answer") String[] test_answer) {
+	public String mark(@ModelAttribute ("questions_id") int[] questions_id, @ModelAttribute("test_answer") String[] test_answer, Model model, Principal principal) {
 		double score = 0.0;
 		List<CorrectAnswer> calist = caService.findAll();
 		//１つ目のfor文ループ:1つの解答欄につき同じ処理を繰り返す
@@ -38,19 +42,49 @@ public class TestController {
 		for (int i = 0; i < test_answer.length; i++) {
 			//2つ目のfor文ループ:テスト問題の持っているquestions_idがcalistのどのquestions_idと一致するかを調べる（？）
 			for (int j = 0; j < questions_id.length; j++) {
-				//question_ids[j] = Integer.parseInt(questions_id[j]);
-				question_ids[j] = ((CorrectAnswer) calist).getQuestionsId();
+				question_ids[j] = getQuestionsId();
+				System.out.println("A");
 				//3つ目のfor文ループ:test_answerとanswerが一致していたら1ポイントゲット
 				for (int k = 0; k < calist.size(); k++) {
 					if (calist.get(k) != null && test_answer[i].equals(calist.get(k).getAnswer())) {
 						score ++;
+						System.out.println("B");
 						break;
 					}
 				}
 			}
 		}
-		System.out.println(score);
+		//ユーザー情報の取得（ユーザー名）
+		Authentication auth = (Authentication) principal;
+		UserDetails userDetails = (UserDetails)auth.getPrincipal();
+		String username = userDetails.getUsername();
+
+		//100点満点中の得点を計算
+		long total_score = Math.round(100 * score /(double)question_ids.length);
+    	int total = (int) total_score;
+
+    	//現在時刻（採点時）を取得
+    	long millis = System.currentTimeMillis();
+		Timestamp timestamp = new Timestamp(millis);
+
+		//総問題数・ユーザー名・正答数・得点・採点時時刻をmark.htmlに送る
+		model.addAttribute("question_ids", question_ids);
+		//System.out.println(question_ids.length);
+		model.addAttribute("username", username);
+		//System.out.println(username);
+		model.addAttribute("score", (int)score);
+		//System.out.println((int)score);
+		model.addAttribute("total", total);
+		//System.out.println(total);
+		model.addAttribute("time", timestamp);
+
+		//DBのhistoriesテーブルに結果を保存
 
 		return "/mark";
+	}
+
+	private int getQuestionsId() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
 	}
 }
