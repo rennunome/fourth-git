770e2830606c4653f282faa9cc942e84a6ec0dc1
rennunome <r_nunome@emyus.jp>
2021-04-06 12:12:02 +0900

    Added function to register multiple answers

--- src/main/java/com/emyus/RegisterController.java
+++ src/main/java/com/emyus/RegisterController.java
@@ -1,5 +1,7 @@
 package com.emyus;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,15 +16,22 @@ public class RegisterController {
 	}
 
 	@PostMapping("/qaregconfirm")
-	public String qaregconfirm(@ModelAttribute("Question") String question, @ModelAttribute("answer") String answer, Model model) {
+	public String qaregconfirm(@ModelAttribute("Question") String question, Model model, HttpServletRequest request) {
+		String[] answer = request.getParameterValues("answer");
+
 		String error_message_question = "";
 		String error_message_answer = "";
 
-		if (question.length() > 500 && answer.length() > 200) {
-			error_message_question = "500文字以上は入力できません。";
-			error_message_answer = "200文字以上は入力できません。";
+		if (question != null && question.length() > 500) {
+			for (int i = 0; i < answer.length; i++) {
+				if (answer[i] != null && answer[i].length() > 200) {
+					error_message_question = "500文字以上は入力できません。";
+					error_message_answer = "200文字以上は入力できません。";
+				}
+			}
 			model.addAttribute("error_message_question", error_message_question);
 			model.addAttribute("error_message_answer", error_message_answer);
+			return "/qaregister";
 		}
 
 		if(question != null && question.length() > 500) {
@@ -30,14 +39,18 @@ public class RegisterController {
 			model.addAttribute("error_message_question", error_message_question);
 			return "/qaregister";
 		}
-		if (answer != null && answer.length() > 200) {
-			error_message_answer = "200文字以上は入力できません。";
-			model.addAttribute("error_message_answer", error_message_answer);
-			return "/qaregister";
+		if (answer != null) {
+			for (int i = 0; i < answer.length; i++) {
+				if (answer[i] != null && answer[i].length() > 200) {
+					error_message_answer = "200文字以上は入力できません。";
+					model.addAttribute("error_message_answer", error_message_answer);
+					return "/qaregister";
+				}
+			}
 		}
-
-		model.addAttribute(question, "Question");
-		model.addAttribute(answer, "answer");
+		model.addAttribute("Question", question);
+		model.addAttribute("answer", answer);
 		return "qaregconfirm";
 	}
 }
+
