4d5cc5bed7881788741f6b6e45cc0b21520fb5e1
rennunome <r_nunome@emyus.jp>
2021-04-07 17:43:10 +0900

    Modified files for QA edit function

--- src/main/java/com/emyus/QADBEditController.java
+++ src/main/java/com/emyus/QADBEditController.java
@@ -1,18 +1,15 @@
 package com.emyus;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.emyus.ca.CARequest;
 import com.emyus.ca.CAService;
-import com.emyus.ca.CorrectAnswer;
-import com.emyus.que.Question;
 import com.emyus.que.QuestionRequest;
 import com.emyus.que.QuestionService;
 @Controller
@@ -22,23 +19,27 @@ public class QADBEditController {
 	private QuestionService questionService;
 
 	@Autowired
-	private CAService CAService;
+	private CAService caService;
 
 	@PostMapping("/qaeditconfirm2")
-	public String DBregister(@ModelAttribute("questions_id") int questions_id, @ModelAttribute("answer_id") int cas_id, @ModelAttribute("question") String question, @ModelAttribute("answer") String answer, Model model , QuestionRequest questionRequest, CARequest caRequest) {
-		questionRequest.setId(questions_id);
-		caRequest.setId(cas_id);
+	public String DBregister(@ModelAttribute("questions_id") int questions_id, @ModelAttribute("answer_id") int cas_id, @ModelAttribute("question") String question, Model model , QuestionRequest questionRequest, CARequest caRequest, HttpServletRequest request) {
 		questionService.update(questionRequest);
-		CAService.update(caRequest);
+		String[] answer = request.getParameterValues("answer");
+		for (int i = 0; i < answer.length; i++) {
+			    if (answer[i] != null) {
+			        caRequest.setAnswer(answer[i]);
+			        caService.update(caRequest);
+			    }
+			};
 		return "redirect:/list";
 	}
 
-	@RequestMapping("/list")
-	public String displayList(Model model) {
-		List<Question> questionlist = questionService.findAll();
-		List<CorrectAnswer> calist = CAService.findAll();
-		model.addAttribute("questionlist", questionlist);
-		model.addAttribute("calist", calist);
-		return "/list";
-	}
+//	@RequestMapping("/list")
+//	public String displayList(Model model) {
+//		List<Question> questionlist = questionService.findAll();
+//		List<CorrectAnswer> calist = caService.findAll();
+//		model.addAttribute("questionlist", questionlist);
+//		model.addAttribute("calist", calist);
+//		return "/list";
+//	}
 }
--- src/main/java/com/emyus/QAEditController.java
+++ src/main/java/com/emyus/QAEditController.java
@@ -1,5 +1,7 @@
 package com.emyus;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +27,7 @@ public class QAEditController {
 	@PostMapping("/qaedit")
 	public String displayEdit(@ModelAttribute("questions_id") int questions_id, @ModelAttribute("id") int cas_id, Model model , QuestionRequest questionRequest, CARequest caRequest) {
 		Question question = questionService.findById(questionRequest);
+		System.out.println("id = " + questionService.findById(questionRequest));
 		CorrectAnswer ca = CAService.findById(caRequest);
 		model.addAttribute("question", question.getQuestion());
 		model.addAttribute("answer", ca.getAnswer());
@@ -34,7 +37,8 @@ public class QAEditController {
 	}
 
 	@PostMapping("/qaeditconfirm")
-	public String displayEditConfirm(@ModelAttribute("questions_id") String questions_id, @ModelAttribute("answer_id") String cas_id, @ModelAttribute("question") String question, @ModelAttribute("answer") String answer, Model model, QuestionRequest questionRequest, CARequest caRequest) {
+	public String displayEditConfirm(@ModelAttribute("questions_id") String questions_id, @ModelAttribute("answer_id") String cas_id, @ModelAttribute("question") String question, Model model, QuestionRequest questionRequest, CARequest caRequest, HttpServletRequest request) {
+		String[] answer = request.getParameterValues("answer");
 		model.addAttribute("question", question);
 		model.addAttribute("answer", answer);
 		model.addAttribute("questions_id", questions_id);
