c08fd3e2f271a482f878f37cf7450c83d0649c06
rennunome <r_nunome@emyus.jp>
2021-04-07 14:52:52 +0900

    Added files for js

--- src/main/resources/static/js/addForm.js
+++ src/main/resources/static/js/addForm.js
@@ -5,8 +5,8 @@ var i = 1 ;
 function addForm() {
   var input_data = document.createElement('input');
   input_data.type = 'text';
-  input_data.id = 'answer_' + i;
-  //input_data.placeholder = 'フォーム-' + i;
+  input_data.id = 'answer' + i;
+input_data.name = 'answer';
   var parent = document.getElementById('form_area');
   parent.appendChild(input_data);
   i++ ;
